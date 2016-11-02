3aabcf8402d3b8ac8a635eeccd1085e7bbeb531f
Felipe Anchieta Santos Costa <felipeanchieta@users.noreply.github.com>
2016-11-01 22:46:46 -0200

    Added some functions and unit tests for them

--- src/client/client.py
+++ src/client/client.py
@@ -1,3 +1,30 @@
+import socket
+
+import sys
+
+
 class TCPClient:
-    def start(self):
-        None
+    def __init__(self):
+        self.is_connected = False
+        self.socket = None
+
+    def connect_socket(self, addr):
+        """Connect to a server given an address of IP and port"""
+        try:
+            self.socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+            self.socket.connect(addr)
+            self.is_connected = True
+        except ConnectionError:
+            self.is_connected = False
+
+    def disconnect_socket(self):
+        if self.is_connected():
+            self.socket.close()
+        else:
+            print('The socket is not connected', file=sys.stderr)
+
+    def send_message(self, msg):
+        if self.is_connected():
+            self.socket.write(msg)
+        else:
+            print('The socket is not connected', file=sys.stderr)
--- test/client/client_test.py
+++ test/client/client_test.py
@@ -1,4 +1,9 @@
+import socket
 import unittest
+
+from mock.mock import MagicMock
+
+from src.client.client import TCPClient
 
 
 class TCPClientTest(unittest.TestCase):
@@ -8,15 +13,50 @@ class TCPClientTest(unittest.TestCase):
     def tearDown(self):
         None
 
-    # -*- Unit Tests -*- #
-    def test_connectionOk(self):
-        self.assertEqual(True, False)
+    def test_socketConnectionSuccessful(self):
+        """Test whether the TCPClient can successfully connect to an open socket"""
+        client = TCPClient()
+        serversocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        serversocket.bind(('localhost', 8080))
+        serversocket.listen()
+        client.connect_socket(('localhost', 8080))
+        self.assertTrue(client.is_connected)
 
-    def test_sendMessageOk(self):
-        self.assertEqual(True, False)
+    def test_socketConnectionFailure(self):
+        """Test whether the TCPClient cannot successfully connect to an closed socket"""
+        client = TCPClient()
+        serversocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        serversocket.bind(('localhost', 8080))
+        self.assertFalse(client.connect_socket(('localhost', 8080)))
 
-    def test_serverUnreachable(self):
-        self.assertEqual(True, False)
+    def test_socketSendMessageSuccessfully(self):
+        """Test whether the TCPClient is capable of sending a message or not"""
+        client = TCPClient()
+        client.is_connected = MagicMock(return_value=True)
+        client.socket = MagicMock()
+        client.send_message('Test')
+        client.socket.write.assert_called_with('Test')
 
-    def test_serverNotResponding(self):
-        self.assertEqual(True, False)
+    def test_socketDoesntSendMessageIfNotConnected(self):
+        """Test wheter the TCPClient tries to send a message without having a open socket or not"""
+        client = TCPClient()
+        client.is_connected = MagicMock(return_value=False)
+        client.socket = MagicMock()
+        client.send_message('Test')
+        client.socket.write.assert_not_called()
+
+    def test_socketSuccessfullyCloses(self):
+        """Test whether the TCPClient is capable of closing its socket"""
+        client = TCPClient()
+        client.is_connected = MagicMock(return_value=True)
+        client.socket = MagicMock()
+        client.disconnect_socket()
+        client.socket.close.assert_called()
+
+    def test_socketDontCloseIfNotOpen(self):
+        """Test whether the TCPClient is capable of closing its socket"""
+        client = TCPClient()
+        client.is_connected = MagicMock(return_value=False)
+        client.socket = MagicMock()
+        client.disconnect_socket()
+        client.socket.close.assert_not_called()
