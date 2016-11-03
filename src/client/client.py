45d4946a027f4b0a8edda7f2195d590c342abc00
Felipe Anchieta Santos Costa <felipeanchieta@hotmail.com.br>
2016-11-02 22:01:12 -0200

    Added client feature

--- src/client/client.py
+++ src/client/client.py
@@ -18,12 +18,14 @@ class TCPClient:
             self.is_connected = False
 
     def disconnect_socket(self):
+        """Close the socket if is open"""
         if self.is_connected():
             self.socket.close()
         else:
             print('The socket is not connected', file=sys.stderr)
 
     def send_message(self, msg):
+        """Sends a message through the open socket"""
         if self.is_connected():
             self.socket.write(msg)
         else:
---
+++ src/tcp_facade.py
@@ -0,0 +1,4 @@
+class TCPFacade():
+    def send_message(self, message):
+        """Sends a message to the server and return True if success"""
+        pass
